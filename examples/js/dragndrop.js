bf346830627c35e7d3129bc694aa147b69c337bc
Tino Fileccia <tinofileccia@yahoo.com>
2018-08-10 02:16:24 -0500

    updated examples

--- examples/js/dragndrop.js
+++ examples/js/dragndrop.js
@@ -1,25 +1,28 @@
-var canvas = document.querySelector( '.a-canvas' );
+window.onload = function () {
+  var canvas = document.querySelector( '.a-canvas' );
 
-canvas.addEventListener('dragover', function( e ) {
-  e.preventDefault();
-})
+  canvas.addEventListener('dragover', function( e ) {
+    e.preventDefault();
+  })
 
-canvas.addEventListener('drop', function( e ) {
+  canvas.addEventListener('drop', function( e ) {
 
-  console.log('File(s) dropped');
+    console.log('File(s) dropped');
 
-  // Prevent default behavior (Prevent file from being opened)
-  e.preventDefault();
+    // Prevent default behavior (Prevent file from being opened)
+    e.preventDefault();
 
-  if ( e.dataTransfer.items ) {
+    if ( e.dataTransfer.items ) {
 
-    if ( e.dataTransfer.items[0].kind === 'file' ) {
-      var file = e.dataTransfer.items[0].getAsFile();
-      var blobURL = window.URL.createObjectURL( file );
-      document.getElementById('audio').setAttribute('src', blobURL);
-      document.querySelector('.songtitle').innerHTML = file.name;
+      if ( e.dataTransfer.items[0].kind === 'file' ) {
+        var file = e.dataTransfer.items[0].getAsFile();
+        var blobURL = window.URL.createObjectURL( file );
+        document.getElementById('audio').setAttribute('src', blobURL);
+        document.querySelector('.songtitle').innerHTML = file.name;
+      }
+
     }
 
-  }
+  });
 
-})
+}
